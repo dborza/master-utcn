fcfea2082f5b4efb061b728aae84bf652d68ed48
Dan Borza <danborza@Dans-MacBook-Pro.local>
2019-06-27 16:26:10 -0700

    Adding cockroach load/run scripts as well

--- src/workflow/run-cassandra.sh
+++ src/workflow/run-cassandra.sh
@@ -8,6 +8,7 @@ echo "Found existing sensor rows #"$SENSOR_ROWS
 MEASUREMENT_ROWS=`cqlsh -e "select count(*) from master.measurement;" | sed -n 4p | awk '{$1=$1};1'`
 echo "Found existing measurement rows #"$MEASUREMENT_ROWS
 
-mvn clean install && ycsb run cassandra-cql -P workloads/workload1 -p hosts="localhost" -p cassandra.keyspace="master" \
+mvn clean install && ycsb run cassandra-cql -P workloads/workload1 \
+-p hosts="localhost" -p cassandra.keyspace="master" \
 -p device_rows=$DEVICE_ROWS -p sensor_rows=$SENSOR_ROWS -p measurement_rows=$MEASUREMENT_ROWS \
 -threads 10 -cp ./target/core-0.16.0-SNAPSHOT.jar
--- src/workflow/run-cockroach.sh
+++ src/workflow/run-cockroach.sh
@@ -8,7 +8,7 @@ echo "Found existing sensor rows #"$SENSOR_ROWS
 MEASUREMENT_ROWS=`cqlsh -e "select count(*) from master.measurement;" | sed -n 4p | awk '{$1=$1};1'`
 echo "Found existing measurement rows #"$MEASUREMENT_ROWS
 
-mvn clean install && ycsb load cassandra-cql -P workloads/workload1 \
--p hosts="localhost" -p cassandra.keyspace="master" \
+mvn clean install && ycsb run jdbc -P workloads/workload1 \
+-p db.driver=org.postgresql.Driver -p db.url=jdbc:postgresql://localhost:26257/master -p db.user=root \
 -p device_rows=$DEVICE_ROWS -p sensor_rows=$SENSOR_ROWS -p measurement_rows=$MEASUREMENT_ROWS \
 -threads 10 -cp ./target/core-0.16.0-SNAPSHOT.jar
